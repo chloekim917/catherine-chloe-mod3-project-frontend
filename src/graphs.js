8f75f978ccfc8ab89990c739929b388f0d04a31a
cathjime <xcatherine.jimenez@gmail.com>
2020-07-08 09:54:10 -0400

    commits before merge

---
+++ src/graphs.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(e){
+    // let popCanvas = document.getElementById("popChart");
+
+    const donutInfoContainer = document.getElementsByTagName("script")[1]
+    const comparisonChartContainer = document.getElementsByTagName("script")[2]
+
+    let donutContent = donutInfoContainer.innerHTML
+    donutContent.innerHTML = 
+    
+
+
+    // const donutChartId = document.getElementsByTagName("script")[1].dataset.id
+    // const ComparisonChartId = document.getElementsByTagName("script")[2].dataset.id
+
+
+})
